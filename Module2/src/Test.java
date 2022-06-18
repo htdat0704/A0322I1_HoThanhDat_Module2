c02451ae3c085dcd82df3fad6b50d2027da924f3
htdat0704 <htdat.18it3@sict.udn.vn>
2022-06-18 18:10:53 +0700

    Commit

--- Module2/src/Test.java
+++ Module2/src/Test.java
@@ -1,4 +1,17 @@
 public class Test {
+    public static boolean isPalindrome (String str) {
+        int count = 0;
+        for(int i =0; i< str.length()/2; i++){
+            if(str.charAt(i)==str.charAt(str.length()-1-i)){
+                count ++;
+            }
+        }
+        if(count == str.length()/2){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     public static int count_even_numbers (int n) {
         if(n ==0 ){
@@ -14,6 +27,9 @@ public class Test {
         }
     }
     public static void main(String args[]) {
+        System.out.println(isPalindrome("abba"));
+        String s = "23";
+        System.out.println(s.charAt(1));
 
         System.out.println(count_even_numbers(5));
 
---
+++ Module2/src/ss8_cleancode/thuc_hanh/variable/FizzBuzz.java
@@ -0,0 +1,18 @@
+package ss8_cleancode.thuc_hanh.variable;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean isBuzz = number % 3 == 0;
+        boolean isFizz = number % 5 == 0;
+        if(isBuzz && isFizz)
+            return "FizzBuzz";
+
+        if(isBuzz)
+            return "Fizz";
+
+        if(isFizz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
