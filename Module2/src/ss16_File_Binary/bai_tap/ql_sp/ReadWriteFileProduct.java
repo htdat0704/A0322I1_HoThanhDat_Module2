0dea189fbc59b9592d894093f95f87df417e67ca
htdat0704 <htdat.18it3@sict.udn.vn>
2022-07-24 14:53:05 +0700

    Commit

--- Module2/src/ss16_File_Binary/bai_tap/ql_sp/ReadWriteFileProduct.java
+++ Module2/src/ss16_File_Binary/bai_tap/ql_sp/ReadWriteFileProduct.java
@@ -3,6 +3,7 @@ package ss16_File_Binary.bai_tap.ql_sp;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ReadWriteFileProduct {
     public static void writeToFile(String path, List<Product> products) {
@@ -33,16 +34,53 @@ public class ReadWriteFileProduct {
 
     public static void main(String[] args) {
         List<Product> products = new ArrayList<>();
-        products.add(new Product(1, "Mát", "Máy quạt","Toshiba",123));
-        products.add(new Product(1, "Mát Lạnh", "Máy quạt lạnh","Sonic",123));
-        products.add(new Product(1, "Tivi ", "TV","LG",123));
-        products.add(new Product(1, "bỏ đổ làm lạnh", "Tủ lạnh","Toshiba",123));
-        products.add(new Product(1, "Mouse", "Chuột","DAREU",123));
         String path  = "src/ss16_File_Binary/bai_tap/ql_sp/product.txt";
-        writeToFile(path, products);
+        Scanner scanner = new Scanner(System.in);
         List<Product> studentDataFromFile = readDataFromFile(path);
-        for (Product product : studentDataFromFile){
-            System.out.println(product);
-        }
+        int choseMenu = -1;
+        do{
+            System.out.println("Enter number: ");
+            System.out.println("1. Display Products");
+            System.out.println("2. Add Product");
+            System.out.println("3. Search");
+            System.out.println("4. End");
+            choseMenu = Integer.parseInt(scanner.nextLine());
+            switch (choseMenu){
+                case 1:
+                    for (Product product : studentDataFromFile){
+                        System.out.println(product);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter id for new Product: ");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter name for new Product: ");
+                    String name = scanner.nextLine();
+                    System.out.println("Enter description of birth for new Product: ");
+                    String description = scanner.nextLine();
+                    System.out.println("Enter brand for new Product : ");
+                    String brand = scanner.nextLine();
+                    System.out.println("Enter price for new Product : ");
+                    double price = Double.parseDouble(scanner.nextLine());
+                    studentDataFromFile.add(new Product(id,description,name,brand,price));
+                    System.out.println("Price add success");
+                    writeToFile(path, products);
+                    break;
+                case 3:
+                    System.out.println("Enter Name you want to search: ");
+                    String nameSearch = scanner.nextLine().trim();
+                    boolean checkNull = false;
+                    for (Product product : studentDataFromFile){
+                        if(nameSearch.equals(product.getName())){
+                            checkNull = true;
+                            System.out.println(product);
+                        }
+                    }
+                    if(!checkNull){
+                        System.out.println("No product Found by name " + nameSearch);
+                    }
+                    break;
+            }
+        }while (choseMenu != 4);
     }
 }
